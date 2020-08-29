3d4ebcc7d7826af4f9fc1bf33ef5a9082b2664c7
drogan120 <alimahmudin.hp@gmail.com>
2020-08-29 21:35:01 +0700

    adding api aon buku for show all data or by id

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Model\Buku;
+use Illuminate\Http\Request;
+
+class BukuController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $buku = Buku::all();
+        return  response()->json(
+            [
+                'buku' => $buku,
+                'response' => 200
+            ]
+        );
+    }
+
+    public function show($id)
+    {
+        $buku = Buku::findOrFail($id);
+        return  response()->json(
+            [
+                'buku' => $buku,
+                'response' => 200
+            ]
+        );
+    }
+}
